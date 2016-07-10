cad086dd945c1a0a63ad8d7fbacb0ecc676adb60
Vladislav Nikonov <pacmancoder@gmail.com>
2016-07-10 13:19:02 +0300

    Second build file fix

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
 extern crate gcc;
 
+use std::env;
 use std::process::Command;
 
 const AYUMI_PATH: &'static str = "ayumi-lib/ayumi.c";
 fn main() {
     // init submodules
-    Command::new("git").arg("submodule").arg("update").output().unwrap_or_else(|e| {
-        panic!("ayumi-lib build failed: {}", e);
-    });
+    Command::new("git").args(&["submodule", "update", "--init", "ayumi-lib"])
+                       .current_dir(env::var("CARGO_MANIFEST_DIR").unwrap())
+                       .output().unwrap();
     // compile lib
     gcc::compile_library("libayumi.a", &[AYUMI_PATH]);
 }
