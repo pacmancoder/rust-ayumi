c1714701af260ceffd2b13e61a73b2a4955bd52b
Vladislav Nikonov <pacmancoder@gmail.com>
2016-07-10 12:42:30 +0300

    Build file fix

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 const AYUMI_PATH: &'static str = "ayumi-lib/ayumi.c";
 fn main() {
     // init submodules
-    Command::new("git").arg("submodule").arg("init").output().unwrap_or_else(|e| {
+    Command::new("git").arg("submodule").arg("update").output().unwrap_or_else(|e| {
         panic!("ayumi-lib build failed: {}", e);
     });
     // compile lib
